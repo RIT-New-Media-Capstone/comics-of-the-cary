4ffc43a1bc657e6fa74e1676ca95aed713e3b793
clh5602 <clh5602@rit.edu>
2024-11-20 18:11:21 -0500

    ready to begin work

--- client/src/js/display-switch.js
+++ client/src/js/display-switch.js
@@ -1,5 +1,15 @@
 import { startCover } from "./comic.js";
-// TODO import { ballsCover } from "./covers/balls.js";
+import { templateCover } from "./covers/template.js";
+import { kubert } from "./covers/kubert.js";
+import { balloonRitchie } from "./covers/balloonRitchie.js";
+import { zapf } from "./covers/zapf.js";
+import { ellsworth } from "./covers/ellsworth.js";
+import { fred } from "./covers/fred.js";
+import { elzbieta } from "./covers/elzbieta.js";
+import { tomaszewski } from "./covers/tomaszewski.js";
+import { kenedy } from "./covers/kenedy.js";
+import { harak } from "./covers/harak.js";
+import { jacqueline } from "./covers/jacqueline.js";
 
 // This function is configured to be called whenever
 // a new comic is scanned.
@@ -9,15 +19,40 @@ import { startCover } from "./comic.js";
 // page to reflect the new comic.
 
 export const handleNewComic = (comicName) => {
-  // this is just a test
-  document.querySelector("p").textContent = comicName;
-
-  // Note: checkout "labels" in model/metadata.json for list of all possible class names
 
   switch (comicName) {
-    // TODO
-    case "balls":
-      startCover(ballsCover);
+    case "Nothing":
+      startCover(templateCover);
+      break;
+    case "kubert":
+      startCover(kubert);
+      break;
+    case "balloonRitchie":
+      startCover(balloonRitchie);
+      break;
+    case "zapf":
+      startCover(zapf);
+      break;
+    case "ellsworth":
+      startCover(ellsworth);
+      break;
+    case "fred":
+      startCover(fred);
+      break;
+    case "elzbieta":
+      startCover(elzbieta);
+      break;
+    case "tomaszewski":
+      startCover(tomaszewski);
+      break;
+    case "kenedy":
+      startCover(kenedy);
+      break;
+    case "harak":
+      startCover(harak);
+      break;
+    case "jacqueline":
+      startCover(jacqueline);
       break;
   }
 };
