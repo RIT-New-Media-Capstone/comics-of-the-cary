90bd67f1c7bac1283b642a115722c01a8d365129
clh5602 <clh5602@rit.edu>
2024-11-20 17:33:02 -0500

    working on getting this stuff merged

---
+++ client/src/js/comic.js
@@ -0,0 +1,76 @@
+const loadImages = async (images) => {
+  const imageElements = {};
+
+  for (const [id, url] of Object.entries(images)) {
+    imageElements[id] = await loadImage(url);
+  }
+
+  return imageElements;
+};
+
+const loadImage = (url) => {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+
+    image.onload = () => {
+      console.log("i guess colby didn't fuck it up");
+      resolve(image);
+    };
+
+    image.onerror = () => {
+      reject(new Error("god fucking damnit colby"));
+    };
+  });
+};
+
+const updateSpeechBubbleText = (text) => {
+  const speechBubble = document.querySelector(".speech-bubble > p");
+  speechBubble.replaceChildren(document.createTextNode(text));
+};
+
+const updateScreenBackground = (url) => {
+  document.body.style.setProperty("--background-url", `url(${url}`);
+};
+
+export const startCover = async (coverOptions) => {
+  const images = await loadImages(coverOptions.images);
+  const canvas = document.querySelector("canvas");
+  if (coverOptions.speechBubble)
+    updateSpeechBubbleText(coverOptions.speechBubble);
+  if (coverOptions.background) updateScreenBackground(coverOptions.background);
+  canvas.width = 1080;
+  canvas.height = 1920;
+  const ctx = canvas.getContext("2d");
+
+  const coverBounds = {
+    left: (78 / 720) * canvas.width,
+    right: (642 / 720) * canvas.width,
+    top: (242 / 1280) * canvas.height,
+    bottom: (1097 / 1280) * canvas.height,
+    width: (642 / 720) * canvas.width - (78 / 720) * canvas.width,
+    height: (1097 / 1280) * canvas.height - (242 / 1280) * canvas.height,
+  };
+  const draw = () => {
+    ctx.save();
+    ctx.translate(
+      coverBounds.left + coverBounds.width * 0.5,
+      coverBounds.top + coverBounds.height * 0.5
+    );
+    ctx.rotate(0.057);
+    ctx.drawImage(
+      images.cover,
+      -coverBounds.width * 0.5,
+      -coverBounds.height * 0.5,
+      coverBounds.width,
+      coverBounds.height
+    );
+    ctx.restore();
+    coverOptions.draw(ctx, images, coverBounds);
+  };
+
+  setInterval(() => {
+    coverOptions.update(30);
+    draw();
+  }, 30);
+};
---
+++ client/src/js/display-switch.js
@@ -0,0 +1,23 @@
+import { startCover } from "./comic.js";
+// TODO import { ballsCover } from "./covers/balls.js";
+
+// This function is configured to be called whenever
+// a new comic is scanned.
+// comicName is a string corresponding to the name of the scanned comic.
+// Implementation is up to you, but a switch statement should work fine.
+// This function should change out what's displayed on the HTML
+// page to reflect the new comic.
+
+export const handleNewComic = (comicName) => {
+  // this is just a test
+  document.querySelector("p").textContent = comicName;
+
+  // Note: checkout "labels" in model/metadata.json for list of all possible class names
+
+  switch (comicName) {
+    // TODO
+    case "balls":
+      startCover(ballsCover);
+      break;
+  }
+};
