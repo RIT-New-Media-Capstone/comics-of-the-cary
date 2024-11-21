c7c3649d27a3af0a382ed28a830ab8f150b62c50
clh5602 <clh5602@rit.edu>
2024-11-21 13:00:10 -0500

    presentation tiem

--- client/src/js/covers/kenedy.js
+++ client/src/js/covers/kenedy.js
@@ -1,6 +1,7 @@
 export const kenedy = {
     // background: "https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_3x4.jpg",
     speechBubble: "Drag across the cover to colorize",
+    background: "../media/kenedy/back.svg",
     images: {
       cover: "./src/media/kenedy/KENEDY-1.png"
     },
