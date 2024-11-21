713712e00d9b13b7b5eb32c2ffb02571181bbf53
MaxwellChu34 <mc2682@rit.edu>
2024-11-21 01:29:01 -0500

    Attempted to work on Freddy Boy

--- client/src/js/covers/fred.js
+++ client/src/js/covers/fred.js
@@ -1,35 +1,56 @@
+
+function currentImage(ctx, images, offsetX, offsetY, scaledWidth, scaledHeight, current) {
+  if(current == 1) {
+    ctx.drawImage(images.fred1, offsetX + 150, offsetY + 300, scaledWidth * 1.05, scaledHeight * 1.05)
+  } else if (current == 2) {
+    ctx.drawImage(images.fred2, offsetX + 150, offsetY + 300, scaledWidth * 1.05, scaledHeight * 1.05)
+  } else if (current == 3) {
+    ctx.drawImage(images.fred3, offsetX + 150, offsetY + 300, scaledWidth * 1.05, scaledHeight * 1.05)
+  } else if (current == 4) {
+    ctx.drawImage(images.fred4, offsetX + 150, offsetY + 300, scaledWidth * 1.05, scaledHeight * 1.05)
+  }
+  
+}
+
+
+
+
 export const fred = {
     // background: "https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_3x4.jpg",
-    speechBubble: "Tap to change the color palette",
+    speechBubble: "Tap to change the color palette!",
     images: {
-      cover: "./src/media/fred/Artboard 2.svg",
-      fred1: "./src/media/fred/Artboard 2.svg",
+      cover: "./src/media/fred/Artboard 2.png",
+      fred1: "./src/media/fred/Artboard 2.png",
+      fred2: "./src/media/fred/Artboard 2 copy 3.png",
+      fred3: "./src/media/fred/Artboard 2 copy 4.png",
+      fred4: "./src/media/fred/Artboard 2 copy 5.png",
     },
     draw: (ctx, images, bounds) => {
-      /*
-      const svgImage = new Image();
-      svgImage.src = images.fred1;
-      svgImage.onload = () => {
-        const imageAspectRatio = svgImage.width / svgImage.height;
-        const boundsAspectRatio = bounds.width / bounds.height;
-        let scaledWidth, scaledHeight;
-        if (imageAspectRatio > boundsAspectRatio) {
-          scaledWidth = bounds.width;
-          scaledHeight = bounds.width / imageAspectRatio;
-        } else {
-          scaledHeight = bounds.height;
-          scaledWidth = bounds.height * imageAspectRatio;
-        }
-        const offsetX = bounds.left + bounds.width * 0.5 - scaledWidth * 0.5;
-        const offsetY = bounds.top + bounds.height * 0.5 - scaledHeight * 0.5;
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.drawImage(svgImage, offsetX, offsetY, scaledWidth, scaledHeight);
-      };
-      */
+      const imageAspectRatio = images.cover.width / images.cover.height;
+      const canvasAspectRatio = bounds.width / bounds.height;
+      let scaledWidth, scaledHeight;
+      if (imageAspectRatio > canvasAspectRatio) {
+        scaledWidth = bounds.width;
+        scaledHeight = bounds.width / imageAspectRatio;
+      } else {
+        scaledHeight = bounds.height;
+        scaledWidth = bounds.height * imageAspectRatio;
+      }
+      const offsetX = (bounds.width - scaledWidth) / 2;
+      const offsetY = (bounds.height - scaledHeight) / 2;
+      let current = 1;
+      
+      ctx.save();
+      ctx.rotate(0.057);
+      ctx.fillStyle = "white";
+      ctx.fillRect(bounds.left + 50, bounds.top - 30, bounds.width, bounds.height);
+      currentImage(ctx, images, offsetX, offsetY, scaledWidth, scaledHeight, current);
+      ctx.restore();
     },
     update: (deltaTime) => {
-      console.log('hi max' + deltaTime);
+
+    },
+    onMouseDown: (x, y) => {
+      
     }
   }
