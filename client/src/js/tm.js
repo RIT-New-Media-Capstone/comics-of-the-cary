d6883cb9d46d5479aab1622991e83f1976eed406
MaxwellChu34 <mc2682@rit.edu>
2024-11-21 01:52:43 -0500

    Continued applying covers

--- client/src/js/tm.js
+++ client/src/js/tm.js
@@ -19,7 +19,7 @@ const URL = "./model/";
 // or make it "" to not force a comic
 // the name should appear exactly as it does in
 // the switch statement of display-switch.js
-const forcedComic = "fred";
+const forcedComic = "balloonRitchie";
 
 let model, webcam, textOutput, maxPredictions;
 
