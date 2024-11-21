e4279832c4410a6c09727e89672d6289c3ff82a8
clh5602 <clh5602@rit.edu>
2024-11-21 12:34:18 -0500

    tomazewski good

--- client/src/js/covers/tomaszewski.js
+++ client/src/js/covers/tomaszewski.js
@@ -1,13 +1,12 @@
 export const tomaszewski = {
     // background: "https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_3x4.jpg",
     speechBubble: "Drag around to explore the layers!",
+    background: "../media/tomaszewski/back.svg",
     images: {
       cover: "./src/media/tomaszewski/background.png"
     },
     draw: (ctx, images, bounds) => {
-      console.log("Travis look out behind you!")
     },
     update: (deltaTime) => {
-      console.log('hi max' + deltaTime);
     }
   }
